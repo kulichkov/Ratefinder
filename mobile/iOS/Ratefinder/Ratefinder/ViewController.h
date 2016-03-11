f4ee0ef36127bcc962ef38e706a5b6c1a36da62b
alexander <fairk19@gmail.com>
2016-03-11 21:55:01 +0300

    Создан проект для xCode для IPhone.

---
+++ mobile/iOS/Ratefinder/Ratefinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ratefinder
+//
+//  Created by Александр on 11.03.16.
+//  Copyright © 2016 Ratefinder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
