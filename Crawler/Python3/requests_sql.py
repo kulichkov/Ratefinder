5b2110b2d7d637094adfd106df8ccfff3db1c9ac
niki <niki@ilovesmile.ru>
2016-03-12 21:45:36 +0500

    Реализовал класс, для конекта и выполнение запросов к базе данных.

---
+++ Crawler/Python3/requests_sql.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+#coding=utf-8
+
+"""
+    Запросы к базе данных
+"""
+
+# Подключаем модуль работы с *.ini фаилам
+from parser_conf_ini import confgini
+# Mysql
+import mysql
+from mysql.connector import Error
+
+
+# Работа с Mysql
+class Mysql():
+    # Иницилизация
+    def __init__(self):
+        configniMysql = confgini()
+        self.ipaddr = configniMysql.get('host', '127.0.0.1')
+        self.port = configniMysql.get('port', '3306')
+        self.db = configniMysql.get('database')
+        self.userName = configniMysql.get('user', 'root')
+        self.password = configniMysql.get('password')
+
+    # Подключаемся
+    def connect(self):
+        try:
+            self.dbconnect = mysql.connector.connect(host=self.ipaddr, port=self.port,
+                    database=self.db,user=self.userName,password=self.password)
+            # Проверяем есть ли конект к базе данных
+            if self.dbconnect.is_connected():
+                print('Сonnection OK.')
+            else:
+                print('Сonnection failed.')
+                exit()
+        except Error as error:
+            print(error)
+            exit()
+
+    # Выборка
+    def execute_select(self, request, *args):
+        self.cursor = self.dbconnect.cursor()
+        self.cursor.execute(request, args)
+        return self.cursor.fetchall()
+
+     # Внесение данных
+    def execute(self, request, *args):
+        self.cursor = self.dbconnect.cursor()
+        self.cursor.execute(request, args)
+
+    # Комитим
+    def commit(self):
+        self.dbconnect.commit()
+
+    # Закрываем
+    def connect_close(self):
+        self.dbconnect.close()
