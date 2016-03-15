78552c44bbd2d6334e54c0eab1e40f0956de87c8
niki <niki@ilovesmile.ru>
2016-03-15 14:28:46 +0500

    Последние изменение

--- Crawler/Python3/requests_url.py
+++ Crawler/Python3/requests_url.py
@@ -7,6 +7,9 @@
 
 import urllib.request
 from urllib.error import URLError
+from os.path import split
+from os import chdir
+import http.cookiejar, urllib.request, urllib.parse
 
 
 # Работа с URL
@@ -14,16 +17,39 @@ class Url():
     # Читаем страницу
     def open(url):
         try:
+            print(url)
             temp = urllib.request.urlopen(url).read()
             temp = temp.decode('utf-8')
+            print('testt')
             return temp
         except URLError:
             return 0
 
+    # Скачиваем
+    def downloader(url, directory=split(__file__)[0]):
+        # Рабочия деректория
+        chdir(directory + '/temp')
+        # Извлекаем из URL имя файла
+        destination = url.rsplit('/',1)[1]
+        # Скачиваем файл
+        urllib.request.urlretrieve(url, destination)
+
     # Проверка существование страницы
     def available(url):
         try:
-            urllib.request.urlopen(url).read()
+            # обработка cookies
+            cookie = http.cookiejar.CookieJar()
+            opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
+            # прикидываемся браузером, не обязательно
+            opener.addheaders = [('User-agent', 'Opera/9.80 (Windows NT 6.1; WOW64; U; ru) Presto/2.10.229 Version/11.62')]
+            # коннект
+            urllib.request.install_opener(opener)
+
+            # авторизация и подсовывание cookies
+            opener.open(url)
+            #urllib.request.urlopen(url).read()
             return 1
         except URLError:
             return 0
+
+print(Url.available('http://lenta.ru/sitemap.xml'))
