26ffaf5226c4d7b885c831c056d1887f11899fe8
niki <niki@ilovesmile.ru>
2016-03-16 19:01:26 +0500

    Пробую приручить ORM.

--- Crawler/Python3/requests_sql.py
+++ Crawler/Python3/requests_sql.py
@@ -5,54 +5,27 @@
     Запросы к базе данных
 """
 
+
 # Подключаем модуль работы с *.ini фаилам
 from parser_conf_ini import confgini
-# Mysql
-import mysql
-from mysql.connector import Error
+# ORM
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, text
 
 
-# Работа с Mysql
-class Mysql():
-    # Иницилизация
-    def __init__(self):
-        configniMysql = confgini()
-        self.ipaddr = configniMysql.get('host', '127.0.0.1')
-        self.port = configniMysql.get('port', '3306')
-        self.db = configniMysql.get('database')
-        self.userName = configniMysql.get('user', 'root')
-        self.password = configniMysql.get('password')
 
-    # Подключаемся
-    def connect(self):
-        try:
-            self.dbconnect = mysql.connector.connect(host=self.ipaddr, port=self.port,
-                    database=self.db,user=self.userName,password=self.password)
-            # Проверяем есть ли конект к базе данных
-            if self.dbconnect.is_connected():
-                print('Сonnection OK.')
-            else:
-                print('Сonnection failed.')
-                exit()
-        except Error as error:
-            print(error)
-            exit()
+dbconnect = create_engine('mysql+mysqlconnector://root:obKRBEa8h45X8sQhtALf@192.168.2.252/database', echo=False)
 
-    # Выборка
-    def execute_select(self, request, *args):
-        self.cursor = self.dbconnect.cursor()
-        self.cursor.execute(request, args)
-        return self.cursor.fetchall()
+class Table():
+    #print('testt')
+    pass
 
-     # Внесение данных
-    def execute(self, request, *args):
-        self.cursor = self.dbconnect.cursor()
-        self.cursor.execute(request, args)
+# for x in dbconnect.execute('SELECT * FROM `Sites`'):
+#     print(x)
+#
+meta = sqlalchemy.MetaData(bind=dbconnect, reflect=True)
+# table = meta.tables['Sites']
+# print(list(dbconnect.execute(table.select(table.c.ID == 2))))
 
-    # Комитим
-    def commit(self):
-        self.dbconnect.commit()
-
-    # Закрываем
-    def connect_close(self):
-        self.dbconnect.close()
+sqlalchemy.orm.Mapper(Table, meta.tables['Sites'])
