4fbeea37e842dcc3b7fd4ec04da48edebed29a93
nikitinilya <nikitin_ilya@outlook.com>
2016-03-17 23:08:13 +0500

    modify class: add cfg connection and SessionFactory

--- Crawler/java/crawler/src/orm/Repository.java
+++ Crawler/java/crawler/src/orm/Repository.java
@@ -1,11 +1,40 @@
 package orm;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.metadata.ClassMetadata;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Nikit on 17.03.2016.
  */
 public class Repository<T> implements RepositoryInterface<T> {
+    private static final SessionFactory ourSessionFactory;
+    private static final ServiceRegistry serviceRegistry;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            ourSessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    private static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
 
     @Override
     public List<T> getAll() {
@@ -25,5 +54,24 @@ public class Repository<T> implements RepositoryInterface<T> {
     @Override
     public boolean delete(T entity) {
         return false;
+    }
+
+    public static void main(final String[] args) throws Exception {
+        final Session session = getSession();
+        try {
+            System.out.println("querying all the managed entities...");
+            final Map metadataMap = session.getSessionFactory().getAllClassMetadata();
+            for (Object key : metadataMap.keySet()) {
+                final ClassMetadata classMetadata = (ClassMetadata) metadataMap.get(key);
+                final String entityName = classMetadata.getEntityName();
+                final Query query = session.createQuery("from " + entityName);
+                System.out.println("executing: " + query.getQueryString());
+                for (Object o : query.list()) {
+                    System.out.println("  " + o);
+                }
+            }
+        } finally {
+            session.close();
+        }
     }
 }
