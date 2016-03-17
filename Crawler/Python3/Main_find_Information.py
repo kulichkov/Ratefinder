5ddddc7b89e614f51ce07d190ed1f4c953ff8971
niki <niki@ilovesmile.ru>
2016-03-17 14:20:08 +0500

    Модуль обхода ссылок и добавление информаций.

--- Crawler/Python3/Main_find_Information.py
+++ Crawler/Python3/Main_find_Information.py
@@ -15,4 +15,44 @@
     ключевых слов для каждой личности, и сохранить результаты в таблице PersonPageRank.
 """
 
+from requests_sql import Mysql
+from parser_files import Html
 
+dictKeywords = {}
+
+quest_3 = 'SELECT `PersonID`, `Name` FROM `Keywords`;'
+quest_4 = 'SELECT `ID`, `Url` FROM `Pages` WHERE `LastScanDate` is Null;'
+
+workMysql = Mysql()
+workMysql.connect()
+
+# Делаем список Ключ: Имена
+for x in workMysql.execute_select(quest_3):
+    #print(x)
+    if dictKeywords.get(x[0]):
+        dictKeywords[x[0]].append(x[1])
+    else:
+        dictKeywords[x[0]] = [x[1],]
+        #print(dictKeywords)
+
+#print(workMysql.execute_select(quest_4))
+
+
+
+for link in workMysql.execute_select(quest_4):
+    print(link)
+    #
+    namePage = link[1][link[1].rfind('/'):]
+    pageExten = namePage[namePage.rfind('.'):]
+
+    #
+    if pageExten == '.xml':
+        print(namePage)
+        pass
+    elif pageExten == '.xml.gz':
+        print(namePage)
+    elif pageExten == '.txt':
+        print(namePage)
+    else:
+        parserHtml = Html(link[1], dictKeywords)
+        print(parserHtml.parser())
