4e1c3119103231b0fbbb8c4eb7c3ec94cb096925
niki <niki@ilovesmile.ru>
2016-03-18 16:13:46 +0500

    Добавил до проверку. Модуль должен запускаться как основной.

--- Crawler/Python3/Main_find_Information.py
+++ Crawler/Python3/Main_find_Information.py
@@ -20,68 +20,82 @@ from datetime import datetime
 from Crawler.Python3.parser_files import *
 
 
-# Словарь для хранения ключевых слов
-dictKeywords = {}
+# Главная функция
+def main():
+    # Словарь для хранения ключевых слов
+    dictKeywords = {}
 
-# Запросы
-quest_3 = 'SELECT `PersonID`, `Name` FROM `Keywords`;'
-quest_4 = 'SELECT `SiteID`, `Url`, `ID` FROM `Pages` WHERE `LastScanDate` is Null;'
-quest_5 = 'INSERT INTO `Pages` (`Url`, `SiteID`) VALUES(%s, %s)'
-quest_5_0 = 'UPDATE `Pages` SET `LastScanDate` = %s WHERE `ID` = %s;'
-quest_6 = 'INSERT INTO `PersonPageRank` (`PersonID`,`PageID`, `Rank`) VALUES(%s, %s, %s)'
+    # Запросы
+    quest_3 = 'SELECT `PersonID`, `Name` FROM `Keywords`;'
+    quest_4 = 'SELECT `SiteID`, `Url`, `ID` FROM `Pages` WHERE `LastScanDate` is Null;'
+    quest_5 = 'INSERT INTO `Pages` (`Url`, `SiteID`) VALUES(%s, %s)'
+    quest_5_0 = 'UPDATE `Pages` SET `LastScanDate` = %s WHERE `ID` = %s;'
+    quest_6 = 'INSERT INTO `PersonPageRank` (`PersonID`,`PageID`, `Rank`) VALUES(%s, %s, %s)'
 
-# Создаем для работы с Mysql
-workMysql = Mysql()
-workMysql.connect()
+    # Создаем для работы с Mysql
+    workMysql = Mysql()
+    workMysql.connect()
 
-# Обновляем столбец "LastScanDate" в таблице "Pages"
-def lastScanDate(PageID):
-    timeStamp = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S")
-    workMysql.execute(quest_5_0, timeStamp, PageID)
+    # Обновляем столбец "LastScanDate" в таблице "Pages"
+    def lastScanDate(PageID):
+        timeStamp = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S")
+        workMysql.execute(quest_5_0, timeStamp, PageID)
 
-# Внесение данных в таблицу "Pages"
-def pages(listTemp, SiteID, PageID):
-    for x in listTemp:
-        lastScanDate(PageID)
-        workMysql.execute(quest_5, x, int(SiteID))
-    workMysql.commit()
+    # Внесение данных в таблицу "Pages"
+    def pages(listTemp, SiteID, PageID):
+        for x in listTemp:
+            #lastScanDate(PageID)
+            workMysql.execute(quest_5, x, int(SiteID))
+        else:
+            lastScanDate(PageID)
+            workMysql.commit()
 
-# Внесение данных в таблицу "PersonPageRank"
-def personPageRank(dictTemp, PageID):
-    lastScanDate(PageID)
-    for x in dictTemp.items():
-        print(x[0], x[1])
-        workMysql.execute(quest_6, int(x[0]), int(PageID), int(x[1]))
-    workMysql.commit()
+    # Внесение данных в таблицу "PersonPageRank"
+    def personPageRank(dictTemp, PageID):
+        #lastScanDate(PageID)
+        for x in dictTemp.items():
+            print(x[0], x[1])
+            workMysql.execute(quest_6, int(x[0]), int(PageID), int(x[1]))
+        else:
+            lastScanDate(PageID)
+            workMysql.commit()
 
-# Делаем список Ключ: Имена
-for x in workMysql.execute_select(quest_3):
-    if dictKeywords.get(x[0]):
-        dictKeywords[x[0]].append(x[1])
+    # Делаем список Ключ: Имена
+    for x in workMysql.execute_select(quest_3):
+        if dictKeywords.get(x[0]):
+            dictKeywords[x[0]].append(x[1])
+        else:
+            dictKeywords[x[0]] = [x[1],]
+
+    # Перебераем ссылки у которых "lastScanDate" = "Null"
+    for link in workMysql.execute_select(quest_4):
+        print(link)
+        #  Узнаем имя файла
+        namePage = link[1][link[1].rfind('/'):]
+        # Узнаем расширение файла
+        pageFormat = namePage[namePage.rfind('.'):]
+
+        # Определение какое рассширение
+        if pageFormat == '.xml':        # Если *.xml
+            pages(Xml.satemap(link[1], pageFormat), link[0], link[2])
+        elif pageFormat == '.gz':       # Если *.gz
+            xmlUrl = Xml.satemap(link[1], pageFormat)
+            if xmlUrl:
+                pages(xmlUrl, link[0], link[2])
+            else:
+                lastScanDate(link[2])
+        elif pageFormat == '.txt':      # Если *.txt
+            robotsUrl = Robots.site_map(link[1])
+            if robotsUrl:
+                pages(robotsUrl, link[0], link[2])
+            else:
+                lastScanDate(link[2])
+        else:                           # Все остальное обыскиваем=)
+            parserHtml = Html(link[1], dictKeywords)
+            personPageRank(parserHtml.parser(), link[2])
     else:
-        dictKeywords[x[0]] = [x[1],]
+        workMysql.connect_close()
 
-# Перебераем ссылки у которых "lastScanDate" = "Null"
-for link in workMysql.execute_select(quest_4):
-    print(link)
-    #  Узнаем имя файла
-    namePage = link[1][link[1].rfind('/'):]
-    # Узнаем расширение файла
-    pageFormat = namePage[namePage.rfind('.'):]
-
-    # Определение какое рассширение
-    if pageFormat == '.xml':
-        pages(Xml.satemap(link[1], pageFormat), link[0], link[2])
-        pass
-    elif pageFormat == '.gz':
-        xmlUrl = Xml.satemap(link[1], pageFormat)
-        if xmlUrl:
-            pages(xmlUrl, link[0], link[2])
-        pass
-    elif pageFormat == '.txt':
-        robotsUrl = Robots.site_map(link[1])
-        if robotsUrl:
-            pages(robotsUrl, link[0], link[2])
-    else:
-        parserHtml = Html(link[1], dictKeywords)
-        personPageRank(parserHtml.parser(), link[2])
+# Проверка
+if __name__ == '__main__':
+    main()
