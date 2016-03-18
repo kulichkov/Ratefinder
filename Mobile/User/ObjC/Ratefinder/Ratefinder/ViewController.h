daa444f576cb6d8a9e8b06880516f3abedf7df99
Mikhail Kulichkov <mikhail.kulichkov@gmail.com>
2016-03-18 21:34:27 +0300

    Создал проект приложения для пользователя

---
+++ Mobile/User/ObjC/Ratefinder/Ratefinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ratefinder
+//
+//  Created by Mikhail Kulichkov on 18/03/16.
+//  Copyright © 2016 Epic Creators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
