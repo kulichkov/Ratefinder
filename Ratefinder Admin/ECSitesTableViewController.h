0957bf568c64b2c9068c5c4bfbd2b0a93b8c415c
alexander <fairk19@gmail.com>
2016-03-18 15:14:19 +0300

    Добавлен класс ECSitesTableViewController для просмотра списка сайтов.

---
+++ Ratefinder Admin/ECSitesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ECSitesTableViewController.h
+//  Ratefinder Admin
+//
+//  Created by Александр on 18.03.16.
+//  Copyright © 2016 Epic-Creators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECSitesTableViewController : UITableViewController
+
+@end
---
+++ Ratefinder Admin/ECSitesTableViewController.m
@@ -0,0 +1,105 @@
+//
+//  ECSitesTableViewController.m
+//  Ratefinder Admin
+//
+//  Created by Александр on 18.03.16.
+//  Copyright © 2016 Epic-Creators. All rights reserved.
+//
+
+#import "ECSitesTableViewController.h"
+
+static NSString *kCellID = @"Cell";
+
+@interface ECSitesTableViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) NSMutableArray *arraySites;
+@property (nonatomic, strong) NSMutableArray *arraySitesUrl;
+
+@end
+
+@implementation ECSitesTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void) viewWillAppear:(BOOL)animated {
+    
+    self.arraySites = [NSMutableArray arrayWithObjects: @"Lenta.ru", @"Взгляд", @"Аргументы и факты", nil];
+    self.arraySitesUrl = [NSMutableArray arrayWithObjects:@"http://www.lenta.ru", @"http://www.vz.ru", @"http://www.aif.ru", nil];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.arraySites count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCellID forIndexPath:indexPath];
+    
+    cell.textLabel.text = [self.arraySites objectAtIndex:indexPath.row];
+    cell.detailTextLabel.text = [self.arraySitesUrl objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
