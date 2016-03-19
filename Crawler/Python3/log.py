db8873e2b0d39acac8812c3fdda53cf8e845e7de
niki <niki@ilovesmile.ru>
2016-03-19 19:11:14 +0500

    Удалил тестовый код. исправил проблемы.

--- Crawler/Python3/log.py
+++ Crawler/Python3/log.py
@@ -6,33 +6,32 @@
 """
 
 import time
+from datetime import datetime
 import os.path
 
 
-# def log1(func):
-#     def wrapper(*args, **kwargs):
-#         workingDir = os.path.split(__file__)[0]
-#         print(workingDir)
-#         nameFileLog = 'crawler.log'
-#         pathLog = workingDir + nameFileLog
-#
-#         #
-#         with open(pathLog, 'a') as file:
-#             #file.write('testttttttt')
-#             print(func.__name__, args, kwargs)
-#             pass
-#
-#     return wrapper
-#
-# def benchmark(func):
-#     """
-#     Декоратор, выводящий время, которое заняло
-#     выполнение декорируемой функции.
-#     """
-#
-#     def wrapper(*args, **kwargs):
-#         t = time.clock()
-#         res = func(*args, **kwargs)
-#         print(func.__name__, time.clock() - t)
-#         return res
-#     return wrapper
+# Логирование в файл
+def logging(event, message):
+    dateTime = str(datetime.utcnow())
+    workingDir = os.path.split(__file__)[0]
+    #print(workingDir)
+    nameFileLog = '/crawler.log'
+    pathLog = workingDir + nameFileLog
+
+    # Открываем на  до запись.
+    with open(pathLog, 'a') as file:
+        file.write('{}: {} >> {}\n'.format(dateTime, event, message))
+
+# Подсчет производительности компонентов
+def benchmark(func):
+    """
+    Декоратор, выводящий время, которое заняло
+    выполнение декорируемой функции.
+    """
+    def wrapper(*args, **kwargs):
+        t = time.clock()
+        res = func(*args, **kwargs)
+        #print(func.__name__, time.clock() - t)
+        logging('Benchmark (' + func.__name__ + ')', time.clock() - t)
+        return res
+    return wrapper
