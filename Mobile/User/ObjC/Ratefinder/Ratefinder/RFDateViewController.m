9639c00796ac889331a9b78549d3b8d8dc439100
Mikhail Kulichkov <mikhail.kulichkov@gmail.com>
2016-03-23 17:01:54 +0300

    Добавление контроллера для выбора даты

---
+++ Mobile/User/ObjC/Ratefinder/Ratefinder/RFDateViewController.m
@@ -0,0 +1,13 @@
+//
+//  RFDateViewController.m
+//  Ratefinder
+//
+//  Created by Mikhail Kulichkov on 23/03/16.
+//  Copyright © 2016 Epic Creators. All rights reserved.
+//
+
+#import "RFDateViewController.h"
+
+@implementation RFDateViewController
+
+@end
