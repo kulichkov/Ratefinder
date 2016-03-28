e2408602bb02ce6e943414d8a8b50f8df1c73de8
Mikhail Kulichkov <mikhail.kulichkov@gmail.com>
2016-03-28 17:27:38 +0300

    Создан класс для коннекта с веб-сервисом.

---
+++ Mobile/User/ObjC/Ratefinder/Ratefinder/RFDatabaseConnection.m
@@ -0,0 +1,13 @@
+//
+//  RFDatabaseConnection.m
+//  Ratefinder
+//
+//  Created by Mikhail Kulichkov on 28/03/16.
+//  Copyright © 2016 Epic Creators. All rights reserved.
+//
+
+#import "RFDatabaseConnection.h"
+
+@implementation RFDatabaseConnection
+
+@end
